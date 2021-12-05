639cee651c691dee026f1d1f4671739177ecdfee
DionisSerbin <reaganusm@gmail.com>
2021-12-05 22:00:19 +0300

    Scripted auto-commit on change (2021-12-05 22:00:19) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/StorageServer.java
+++ src/main/java/ru/bmstu/iu9/StorageServer.java
@@ -1,9 +1,15 @@
 package ru.bmstu.iu9;
 
+import java.util.List;
+
 public class StorageServer {
     private List<String> servers;
 
-    StoreServer(List<String> servers) {
+    StorageServer(List<String> servers) {
         this.servers = servers;
+    }
+
+    public List<String> getServers() {
+        return this.servers;
     }
 }
