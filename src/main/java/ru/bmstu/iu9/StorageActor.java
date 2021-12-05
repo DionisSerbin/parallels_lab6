61142cdc5a443b93ae450076f6ca54ba9a7dc573
DionisSerbin <reaganusm@gmail.com>
2021-12-05 21:58:52 +0300

    Scripted auto-commit on change (2021-12-05 21:58:52) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/StorageActor.java
+++ src/main/java/ru/bmstu/iu9/StorageActor.java
@@ -3,13 +3,15 @@ package ru.bmstu.iu9;
 import akka.actor.AbstractActor;
 
 import java.util.List;
+import java.util.Random;
 
 public class StorageActor extends AbstractActor {
 
     private List<String> servers;
+    private Random random = new Random();
 
     @Override
     public Receive createReceive() {
-        return null;
+        return 
     }
 }
