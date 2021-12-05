a897db2a9cf835efc23cc4adc0a26d89fadf405f
DionisSerbin <reaganusm@gmail.com>
2021-12-05 22:19:23 +0300

    Scripted auto-commit on change (2021-12-05 22:19:23) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/ZooServer.java
+++ src/main/java/ru/bmstu/iu9/ZooServer.java
@@ -1,17 +1,21 @@
 package ru.bmstu.iu9;
 
 import akka.actor.ActorRef;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class ZooServer implements Watcher {
     final private static String SERVERS = "/servers";
     private ZooKeeper zoo;
     private ActorRef storage;
+
+    public ZooServer(ZooKeeper zoo, ActorRef storage) throws KeeperException, InterruptedException {
+        this.zoo = zoo;
+        this.storage = storage;
+        sendServers();
+    }
 
     @Override
     public void process(WatchedEvent watchedEvent) {
@@ -27,6 +31,19 @@ public class ZooServer implements Watcher {
         List<String> servers = zoo.getChildren(SERVERS, this);
         storage.tell(
                 new StorageServer(servers),
+                ActorRef.noSender()
+        );
+    }
+
+    public void createServer(String localhost, String port) throws KeeperException, InterruptedException {
+        zoo.create(
+                SERVERS + "/" + localhost + ":" + port,
+                port.getBytes(StandardCharsets.UTF_8),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL
+        );
+        storage.tell(
+                new FollowingServer(localhost + ":" + port),
                 ActorRef.noSender()
         );
     }
