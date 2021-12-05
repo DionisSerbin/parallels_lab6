3346e0b29ea61ef8682a029df7c27e0d575bc18e
DionisSerbin <reaganusm@gmail.com>
2021-12-05 22:10:08 +0300

    Scripted auto-commit on change (2021-12-05 22:10:08) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/ZookeeperApp.java
+++ src/main/java/ru/bmstu/iu9/ZookeeperApp.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 public class ZookeeperApp {
     final private static String ZOO_HOST = "127.0.0.1:2181";
     final private static int TIME_OUT = 2500;
+    final private static String LOCAL_HOST = "localhost";
     
     public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
         BasicConfigurator.configure();
