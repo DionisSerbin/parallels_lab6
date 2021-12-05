d1f2ac87b97e19ff53555d610d4b1941be235292
DionisSerbin <reaganusm@gmail.com>
2021-12-05 22:02:54 +0300

    Scripted auto-commit on change (2021-12-05 22:02:54) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/FollowingServer.java
+++ src/main/java/ru/bmstu/iu9/FollowingServer.java
@@ -2,4 +2,12 @@ package ru.bmstu.iu9;
 
 public class FollowingServer {
     private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public FollowingServer(String url){
+        this.url = url;
+    }
 }
