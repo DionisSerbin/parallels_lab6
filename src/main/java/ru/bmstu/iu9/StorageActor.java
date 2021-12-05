b61a65163bdfeb1fe0b17aac033107ac676986ac
DionisSerbin <reaganusm@gmail.com>
2021-12-05 21:58:35 +0300

    Scripted auto-commit on change (2021-12-05 21:58:35) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/StorageActor.java
+++ src/main/java/ru/bmstu/iu9/StorageActor.java
@@ -2,9 +2,11 @@ package ru.bmstu.iu9;
 
 import akka.actor.AbstractActor;
 
+import java.util.List;
+
 public class StorageActor extends AbstractActor {
 
-    
+    private List<String> servers;
 
     @Override
     public Receive createReceive() {
