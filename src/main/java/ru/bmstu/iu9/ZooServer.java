930fdc60b45e9b4026f357a7843817e7865cf77e
DionisSerbin <reaganusm@gmail.com>
2021-12-05 22:13:43 +0300

    Scripted auto-commit on change (2021-12-05 22:13:43) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/ZooServer.java
+++ src/main/java/ru/bmstu/iu9/ZooServer.java
@@ -1,5 +1,6 @@
 package ru.bmstu.iu9;
 
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
@@ -10,6 +11,13 @@ public class ZooServer implements Watcher {
     @Override
     public void process(WatchedEvent watchedEvent) {
         System.out.println(watchedEvent.toString());
-        
+        try {
+            sendServers();
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendServers() {
     }
 }
