ebc8a37c70c61d68f65f8004359f4f22c8a0e73f
DionisSerbin <reaganusm@gmail.com>
2021-12-05 21:57:16 +0300

    Scripted auto-commit on change (2021-12-05 21:57:16) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/ZookeeperApp.java
+++ src/main/java/ru/bmstu/iu9/ZookeeperApp.java
@@ -24,6 +24,7 @@ public class ZookeeperApp {
 
     public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
         BasicConfigurator.configure();
+        ActorSystem system = ActorSystem.create("routes");
         
     }
 
