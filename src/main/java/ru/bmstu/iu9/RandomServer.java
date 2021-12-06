873d5d5e96b13b7bc8fb56002b1807eab0829311
DionisSerbin <reaganusm@gmail.com>
2021-12-06 18:44:56 +0300

    Scripted auto-commit on change (2021-12-06 18:44:56) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/RandomServer.java
+++ src/main/java/ru/bmstu/iu9/RandomServer.java
@@ -5,12 +5,4 @@ import java.util.Random;
 public class RandomServer {
     private Random random;
 
-    public Random getRandom() {
-        return random;
-    }
-
-    public RandomServer(Random random){
-        this.random = random;
-    }
-
 }
