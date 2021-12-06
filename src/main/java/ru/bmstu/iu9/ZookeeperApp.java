9b2bc67183ba0473242f1ba5323b1f85057f3385
DionisSerbin <reaganusm@gmail.com>
2021-12-06 18:31:09 +0300

    Scripted auto-commit on change (2021-12-06 18:31:09) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/ZookeeperApp.java
+++ src/main/java/ru/bmstu/iu9/ZookeeperApp.java
@@ -21,6 +21,9 @@ import org.apache.zookeeper.ZooKeeper;
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
+import static akka.http.javadsl.server.Directives.pathSingleSlash;
+import static akka.http.javadsl.server.Directives.route;
+
 public class ZookeeperApp {
     final private static String ZOO_HOST = "127.0.0.1:2181";
     final private static int TIME_OUT = 2500;
@@ -65,7 +68,8 @@ public class ZookeeperApp {
     }
 
     private static Route createRoute(ActorRef storage, final Http http) {
-
+        return route(pathSingleSlash(() ->
+                
     }
 
 }
